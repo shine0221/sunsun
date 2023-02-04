5154b563b5232ea6c720dce4d5f3523f0b0b7ba2
shine0221 <royee13028@gmail.com>
2023-02-04 15:46:57 +0800

    02041546

--- _js/index.js
+++ _js/index.js
@@ -113,24 +113,80 @@ $(document).ready(function(){
         $(".change_comic_1").css('display', 'block');
     });
 
-    $('.Q_1_ans').click(function(){
+    var q1 = document.querySelector('.Q_1_ans p');
+    var q2 = document.querySelector('.Q_2_ans p');
+    var q3 = document.querySelector('.Q_3_ans p');
+    var q4 = document.querySelector('.Q_4_ans p');
+    var q5 = document.querySelector('.Q_5_ans p');
+
+    $('.Q_1').click(function(){
         $('.Q_1_wer').toggle();
+        q1.style.color = "#ED974A";
+        $(".Q_11").css('display', 'block');
+        $(".Q_1").css('display', 'none');
     });
 
-    $('.Q_2_ans').click(function(){
+    $('.Q_11').click(function(){
+        $('.Q_1_wer').toggle();
+        q1.style.color = "black";
+        $(".Q_1").css('display', 'block');
+        $(".Q_11").css('display', 'none');
+    });
+
+    $('.Q_2').click(function(){
         $('.Q_2_wer').toggle();
+        q2.style.color = "#ED974A";
+        $(".Q_22").css('display', 'block');
+        $(".Q_2").css('display', 'none');
     });
 
-    $('.Q_3_ans').click(function(){
+    $('.Q_22').click(function(){
+        $('.Q_2_wer').toggle();
+        q2.style.color = "black";
+        $(".Q_2").css('display', 'block');
+        $(".Q_22").css('display', 'none');
+    });
+
+    $('.Q_3').click(function(){
         $('.Q_3_wer').toggle();
+        q3.style.color = "#ED974A";
+        $(".Q_33").css('display', 'block');
+        $(".Q_3").css('display', 'none');
     });
 
-    $('.Q_4_ans').click(function(){
+    $('.Q_33').click(function(){
+        $('.Q_3_wer').toggle();
+        q3.style.color = "black";
+        $(".Q_3").css('display', 'block');
+        $(".Q_33").css('display', 'none');
+    });
+
+    $('.Q_4').click(function(){
         $('.Q_4_wer').toggle();
+        q4.style.color = "#ED974A";
+        $(".Q_44").css('display', 'block');
+        $(".Q_4").css('display', 'none');
     });
 
-    $('.Q_5_ans').click(function(){
+    $('.Q_44').click(function(){
+        $('.Q_4_wer').toggle();
+        q4.style.color = "black";
+        $(".Q_4").css('display', 'block');
+        $(".Q_44").css('display', 'none');
+    });
+
+    $('.Q_5').click(function(){
         $('.Q_5_wer').toggle();
+        q5.style.color = "#ED974A";
+        $(".Q_55").css('display', 'block');
+        $(".Q_5").css('display', 'none');
+    });
+
+    $('.Q_55').click(function(){
+        $('.Q_5_wer').toggle();
+        q5.style.color = "black";
+        $(".Q_5").css('display', 'block');
+        $(".Q_55").css('display', 'none');
     });
 
     $('.choose_1').click(function(){
@@ -147,79 +203,78 @@ $(document).ready(function(){
         }
       });
     
-      // 點擊按鈕回頂部
       $(".back-top").on("click", function (event) {
         $("html, body").animate(
           {
             scrollTop: 0
           },
-          500 // 回頂部時間為 500 毫秒
+          500 
         );
       });
 
-    var how1 = document.querySelector('#how1');
-    var how11 = document.querySelector('#how11');
-    var how2 = document.querySelector('#how2');
-    var how22 = document.querySelector('#how22');
-    var how3 = document.querySelector('#how3');
-    var how33 = document.querySelector('#how33');
-    var how4 = document.querySelector('#how4');
-    var how44 = document.querySelector('#how44');
-    var how5 = document.querySelector('#how5');
-    var how55 = document.querySelector('#how55');
-
-    var mation1 = document.querySelector('#mation1');
-    var mation11 = document.querySelector('#mation11');
-    var mation2 = document.querySelector('#mation2');
-    var mation22 = document.querySelector('#mation22');
-    var mation3 = document.querySelector('#mation3');
-    var mation33 = document.querySelector('#mation33');
-    var mation4 = document.querySelector('#mation4');
-    var mation44 = document.querySelector('#mation44');
-
-    function to(toEl){
-        let bridge = toEl;
-        let body = document.body;
-        let height = 0;
-
-        do{
-            height += bridge.offsetTop;
-            bridge = bridge.offsetParent;
-        }while (bridge !== body)
-
-        window.scrollTo({
-            top: height,
-            behavior: 'smooth'
-        })
-    }
-
-    how11.addEventListener('click', function(){
-        to(how1)
-    });
-    how22.addEventListener('click', function(){
-        to(how2)
-    });
-    how33.addEventListener('click', function(){
-        to(how3)
-    });
-    how44.addEventListener('click', function(){
-        to(how4)
-    });
-    how55.addEventListener('click', function(){
-        to(how5)
-    });
-    mation11.addEventListener('click', function(){
-        to(mation1)
-    });
-    mation22.addEventListener('click', function(){
-        to(mation2)
-    });
-    mation33.addEventListener('click', function(){
-        to(mation3)
-    });
-    mation44.addEventListener('click', function(){
-        to(mation4)
-    });
+      /*var how1 = document.querySelector('#how1');
+      var how11 = document.querySelector('#how11');
+      var how2 = document.querySelector('#how2');
+      var how22 = document.querySelector('#how22');
+      var how3 = document.querySelector('#how3');
+      var how33 = document.querySelector('#how33');
+      var how4 = document.querySelector('#how4');
+      var how44 = document.querySelector('#how44');
+      var how5 = document.querySelector('#how5');
+      var how55 = document.querySelector('#how55');
+  
+      var mation1 = document.querySelector('#mation1');
+      var mation11 = document.querySelector('#mation11');
+      var mation2 = document.querySelector('#mation2');
+      var mation22 = document.querySelector('#mation22');
+      var mation3 = document.querySelector('#mation3');
+      var mation33 = document.querySelector('#mation33');
+      var mation4 = document.querySelector('#mation4');
+      var mation44 = document.querySelector('#mation44');
+  
+      function to(toEl){
+          let bridge = toEl;
+          let body = document.body;
+          let height = 0;
+  
+          do{
+              height += bridge.offsetTop;
+              bridge = bridge.offsetParent;
+          }while (bridge !== body)
+  
+          window.scrollTo({
+              top: height,
+              behavior: 'smooth'
+          })
+      }
+  
+      how11.addEventListener('click', function(){
+          to(how1)
+      });
+      how22.addEventListener('click', function(){
+          to(how2)
+      });
+      how33.addEventListener('click', function(){
+          to(how3)
+      });
+      how44.addEventListener('click', function(){
+          to(how4)
+      });
+      how55.addEventListener('click', function(){
+          to(how5)
+      });
+      mation11.addEventListener('click', function(){
+          to(mation1)
+      });
+      mation22.addEventListener('click', function(){
+          to(mation2)
+      });
+      mation33.addEventListener('click', function(){
+          to(mation3)
+      });
+      mation44.addEventListener('click', function(){
+          to(mation4)
+      });*/
 });
 
 x=1;
